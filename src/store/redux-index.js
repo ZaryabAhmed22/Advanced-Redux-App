657e5ed9f1c3abdf4d92566d78fb4c44b767c2ec
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-04-08 15:25:09 +0500

    Added cart toggling functionality

---
+++ src/store/redux-index.js
@@ -0,0 +1,23 @@
+const { createSlice, configureStore } = require("@reduxjs/toolkit");
+
+const initalCartState = { showCart: false };
+
+export const uiSlice = createSlice({
+  name: "ui",
+  initialState: initalCartState,
+  reducers: {
+    toggleCart(state) {
+      state.showCart = !state.showCart;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    ui: uiSlice.reducer,
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
+export default store; // >> this store is provided in the Provider component as a store prop.
