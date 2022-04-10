50f7c119e02cc42d6a7a592f96d36da8f7ebc698
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-04-10 17:11:41 +0500

    Sending an http request

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,19 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.showCart);
+
+  //importing whole cart from redux
+  const cart = useSelector((state) => state.cart);
+
+  //Adding side Effects for sending http requests
+  //1. PUT request so the data wil be overwritten
+  //2. Setting cart as a depencency so the effect runs whenever the cart changes.
+  useEffect(() => {
+    fetch("https://redux-e8d5e-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
